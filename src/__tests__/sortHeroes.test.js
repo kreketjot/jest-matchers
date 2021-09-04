227641e516be2ad6cf55ebbbc4ebca54846b8387
Andrey Vinokurov <kreketjot@gmail.com>
2021-09-05 00:27:45 +0300

    renamed appveyor and wrong test added

--- src/__tests__/sortHeroes.test.js
+++ src/__tests__/sortHeroes.test.js
@@ -12,7 +12,7 @@ const sortedHeroes = [
 ];
 const smth = [{ health: 1 }, 'a', {}];
 
-// test('normal sort heroes with toBe', () => expect(sortHeroes(heroes)).toBe(sortedHeroes));
+test('normal sort heroes with toBe', () => expect(sortHeroes(heroes)).toBe(sortedHeroes));
 test('normal sort heroes with toEqual', () => expect(sortHeroes(heroes)).toEqual(sortedHeroes));
 test('abnormal sort heroes', () => expect(sortHeroes()).toEqual([]));
 test('abnormal sort heroes', () => expect(sortHeroes(smth)).toEqual(smth));
